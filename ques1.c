82337273ace41d58f5be50eec9789a2aa57f9097
Rohit Das <rohitdas1996@gmail.com>
2017-04-03 16:16:37 +0530

    Update ques1.c

--- ques1.c
+++ ques1.c
@@ -6,9 +6,31 @@
 int nextState(char *pattern, int M, int state, int x)
 {
   
+ // If the character c is same as next character in pattern,
+  // then simply increment state
+  if (state < M && x == pattern[state])
+  return state+1;
+ 
+  int ns, i; // ns stores the result which is next state
+ 
+  // ns finally contains the longest prefix which is also suffix
+  // in "pattern[0..state-1]c"
+ 
   // Start from the largest possible value and stop when you find
   // a prefix which is also suffix
-  // Code for this function is yet to be written.
+  for (ns = state; ns > 0; ns--)
+  {
+  if(pattern[ns-1] == x)
+  {
+  for(i = 0; i < ns-1; i++)
+  {
+    if (pattern[i] != pattern[state-ns+1+i])
+  break;
+  }
+  if (i == ns-1)
+  return ns;
+  }
+  }
  
   return 0;
 }
@@ -33,7 +55,15 @@ void search(char *pattern, char *txt)
  
   computeTF(pattern, M, TF);
  
-  // Code to process the string over Finite Automata yet to be written .
+  int i, state=0;
+  for (i = 0; i < N; i++)
+  {
+  state = TF[state][txt[i]];
+  if (state == M)
+  {
+  printf ("\n Pattern found at index %d", i-M+1);
+  }
+  }
   
 }
  
